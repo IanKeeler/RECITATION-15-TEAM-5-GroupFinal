05d92ea48ce1bbe4127581c36ecf2f0cc994e539
Ethan Nguyen <etng4880@colorado.edu>
2023-04-16 22:14:52 -0600

    fixing typo in comments to create.sql

--- init_data/create.sql
+++ init_data/create.sql
@@ -81,7 +81,7 @@ CREATE TABLE electricity(
 -- unhashed passwords (respectively)
 -- pass1
 -- pass2
--- pass 3
+-- pass3
 INSERT INTO users (username, user_password) VALUES ('user1', '$2b$10$4KoZfQeiD9MDlI5YRzMZtuBIJbXgjz.QqsZRmDG4NCfsXPSW4APWm');
 INSERT INTO users (username, user_password) VALUES ('user2', '$2b$10$yPeUvrbER1W.Y/NGiffn8usAZFmltMhFB1BFo15jaBUX2dBcKzK76');
 INSERT INTO users (username, user_password) VALUES ('user3', '$2b$10$SpdLRI.7Eb7qF94c44nI4eMWqTf75mHGqGTYb8X3FEZR0yAX/fp/6');
