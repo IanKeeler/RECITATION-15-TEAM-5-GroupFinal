888bfc40508d71c901f229d21a858d5782559676
Ben Gardner <bgardner1205@gmail.com>
2023-04-18 08:49:35 -0600

    this time properly adding changes to create.sql to resolve incorrect datatype for emissions column of freight table

--- init_data/create.sql
+++ init_data/create.sql
@@ -35,7 +35,7 @@ CREATE TABLE freight(
   freight_mode VARCHAR(45) NOT NULL,
   freight_weight INT NOT NULL,
   freight_distance INT NOT NULL,
-  emissions VARCHAR(45) NOT NULL,
+  emissions INT NOT NULL,
   date DATE NOT NULL,
   user_id INT NOT NULL,
   CONSTRAINT user_id
@@ -71,3 +71,18 @@ INSERT INTO users (username, user_password) VALUES ('user1', '$2b$10$4KoZfQeiD9M
 INSERT INTO users (username, user_password) VALUES ('user2', '$2b$10$yPeUvrbER1W.Y/NGiffn8usAZFmltMhFB1BFo15jaBUX2dBcKzK76');
 INSERT INTO users (username, user_password) VALUES ('user3', '$2b$10$SpdLRI.7Eb7qF94c44nI4eMWqTf75mHGqGTYb8X3FEZR0yAX/fp/6');
 
+-- -----------------------------------------------------
+-- test data for leaderboard
+-- -----------------------------------------------------
+
+INSERT INTO travel (travel_mode, travel_distance, emissions, date, user_id) VALUES ('car', 16, 20, '2023-04-01', 1);
+INSERT INTO travel (travel_mode, travel_distance, emissions, date, user_id) VALUES ('car', 16, 10, '2023-04-01', 2);
+INSERT INTO travel (travel_mode, travel_distance, emissions, date, user_id) VALUES ('car', 16, 5, '2023-04-01', 3);
+
+INSERT INTO freight (freight_mode, freight_weight, freight_distance, emissions, date, user_id) VALUES ('road', 15, 25, 30, '2023-04-01', 1);
+INSERT INTO freight (freight_mode, freight_weight, freight_distance, emissions, date, user_id) VALUES ('road', 15, 25, 15, '2023-04-01', 2);
+INSERT INTO freight (freight_mode, freight_weight, freight_distance, emissions, date, user_id) VALUES ('road', 15, 25, 7, '2023-04-01', 3);
+
+INSERT INTO electricity (electricity_mode, electricity_used, emissions, date, user_id) VALUES ('charge device', 25, 10, '2023-04-01', 1);
+INSERT INTO electricity (electricity_mode, electricity_used, emissions, date, user_id) VALUES ('charge device', 25, 5, '2023-04-01', 2);
+INSERT INTO electricity (electricity_mode, electricity_used, emissions, date, user_id) VALUES ('charge device', 25, 2, '2023-04-01', 3);
