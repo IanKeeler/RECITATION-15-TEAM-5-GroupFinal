56f1984b9bb361e3ca31cbf8028e7c190a2f6f59
Ben Gardner <bgardner1205@gmail.com>
2023-04-20 13:22:23 -0600

    fixed incompoatibility of create.sql

--- init_data/create.sql
+++ init_data/create.sql
@@ -6,6 +6,8 @@ CREATE TABLE users(
   user_id SERIAL PRIMARY KEY,
   username VARCHAR(100) NOT NULL,
   user_password VARCHAR(300) NOT NULL,
+  user_weightfactor FLOAT NOT NULL,
+  user_carbonscore FLOAT NOT NULL,
   CONSTRAINT "username_unique" UNIQUE ("username")
 );
 
@@ -28,7 +30,7 @@ CREATE TABLE travel(
 -- -----------------------------------------------------
 -- Table freight
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS freight CASCADE;
+/* DROP TABLE IF EXISTS freight CASCADE;
 CREATE TABLE freight(
   freight_id SERIAL PRIMARY KEY,
   freight_mode VARCHAR(45) NOT NULL,
@@ -40,12 +42,12 @@ CREATE TABLE freight(
   CONSTRAINT user_id
     FOREIGN KEY (user_id)
     REFERENCES users(user_id)
-);
+); */
 
 -- -----------------------------------------------------
 -- Table electricity
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS electricity CASCADE;
+/* DROP TABLE IF EXISTS electricity CASCADE;
 CREATE TABLE electricity(
   electricity_id SERIAL PRIMARY KEY,
   electricity_mode VARCHAR(45) NOT NULL,
@@ -56,7 +58,7 @@ CREATE TABLE electricity(
   CONSTRAINT user_id
     FOREIGN KEY (user_id)
     REFERENCES users(user_id)
-);
+); */
 
 -- -----------------------------------------------------
 -- initialize test users: currently only here for login/registration
@@ -66,9 +68,9 @@ CREATE TABLE electricity(
 -- pass1
 -- pass2
 -- pass3
-INSERT INTO users (username, user_password) VALUES ('user1', '$2b$10$4KoZfQeiD9MDlI5YRzMZtuBIJbXgjz.QqsZRmDG4NCfsXPSW4APWm');
-INSERT INTO users (username, user_password) VALUES ('user2', '$2b$10$yPeUvrbER1W.Y/NGiffn8usAZFmltMhFB1BFo15jaBUX2dBcKzK76');
-INSERT INTO users (username, user_password) VALUES ('user3', '$2b$10$SpdLRI.7Eb7qF94c44nI4eMWqTf75mHGqGTYb8X3FEZR0yAX/fp/6');
+INSERT INTO users (username, user_password, user_weightfactor, user_carbonscore) VALUES ('user1', '$2b$10$4KoZfQeiD9MDlI5YRzMZtuBIJbXgjz.QqsZRmDG4NCfsXPSW4APWm', 1, 50);
+INSERT INTO users (username, user_password, user_weightfactor, user_carbonscore) VALUES ('user2', '$2b$10$yPeUvrbER1W.Y/NGiffn8usAZFmltMhFB1BFo15jaBUX2dBcKzK76', 1, 27);
+INSERT INTO users (username, user_password, user_weightfactor, user_carbonscore) VALUES ('user3', '$2b$10$SpdLRI.7Eb7qF94c44nI4eMWqTf75mHGqGTYb8X3FEZR0yAX/fp/6', 1, 16.8);
 
 -- -----------------------------------------------------
 -- test data for leaderboard
@@ -78,10 +80,10 @@ INSERT INTO travel (travel_mode, travel_distance, emissions, date, user_id) VALU
 INSERT INTO travel (travel_mode, travel_distance, emissions, date, user_id) VALUES ('car', 16, 10, '2023-04-01', 2);
 INSERT INTO travel (travel_mode, travel_distance, emissions, date, user_id) VALUES ('car', 16, 5, '2023-04-01', 3);
 
-INSERT INTO freight (freight_mode, freight_weight, freight_distance, emissions, date, user_id) VALUES ('road', 15, 25, 30, '2023-04-01', 1);
+/* INSERT INTO freight (freight_mode, freight_weight, freight_distance, emissions, date, user_id) VALUES ('road', 15, 25, 30, '2023-04-01', 1);
 INSERT INTO freight (freight_mode, freight_weight, freight_distance, emissions, date, user_id) VALUES ('road', 15, 25, 15, '2023-04-01', 2);
 INSERT INTO freight (freight_mode, freight_weight, freight_distance, emissions, date, user_id) VALUES ('road', 15, 25, 7, '2023-04-01', 3);
 
 INSERT INTO electricity (electricity_mode, electricity_used, emissions, date, user_id) VALUES ('charge device', 25, 10, '2023-04-01', 1);
 INSERT INTO electricity (electricity_mode, electricity_used, emissions, date, user_id) VALUES ('charge device', 25, 5, '2023-04-01', 2);
-INSERT INTO electricity (electricity_mode, electricity_used, emissions, date, user_id) VALUES ('charge device', 25, 2, '2023-04-01', 3);
+INSERT INTO electricity (electricity_mode, electricity_used, emissions, date, user_id) VALUES ('charge device', 25, 2, '2023-04-01', 3); */
