3e744b23197a6077ed8e25332a9b32668792a49d
Ethan Nguyen <etng4880@colorado.edu>
2023-05-02 03:22:58 -0600

    modified log javascript to compensate with UI changes

--- project_code_and_components/resources/js/log.js
+++ project_code_and_components/resources/js/log.js
@@ -1,34 +1,45 @@
 function togglePassengers() {
     const travelMode = document.getElementById("travel_mode").value;
-    const extraInput = document.getElementById("extra_input")
+    const passengersText = document.getElementById("passengers-label");
+    const passengersBox = document.getElementById("passengers");
     if(travelMode == "car" || travelMode == "bus") {
-        extraInput.classList.remove("hidden")
+        passengersText.classList.remove("hidden");
+        passengersBox.classList.remove("hidden");
     } else {
-        extraInput.classList.add("hidden")
+        passengersText.classList.add("hidden");
+        passengersBox.classList.add("hidden");
     }
 }
 function showMode(){
     const inputMode = document.getElementById("input_mode").value;
-    const travelForm = document.getElementById("travel")
-    const electricForm = document.getElementById("household")
-    const freightForm = document.getElementById("food")
-    const outerBox = document.getElementById("outer-box")
+    const travelForm = document.getElementById("travel");
+    const electricForm = document.getElementById("household");
+    const freightForm = document.getElementById("food");
+    const outerBox = document.getElementById("outer-box");
+    const resultsWrapper = document.getElementById("log-results-wrapper");
+    const starterPrompt = document.getElementById("starter-prompt");
     if(inputMode == "travel"){
-        travelForm.classList.remove("hidden")
-        electricForm.classList.add("hidden")
-        freightForm.classList.add("hidden")
-        outerBox.style.display = ""
+        travelForm.classList.remove("hidden");
+        electricForm.classList.add("hidden");
+        freightForm.classList.add("hidden");
+        starterPrompt.classList.add("hidden");
+        outerBox.style.display = "";
+        resultsWrapper.style.display = "";
     }
     if(inputMode == "household"){
-        travelForm.classList.add("hidden")
-        electricForm.classList.remove("hidden")
-        freightForm.classList.add("hidden")
-        outerBox.style.display = ""
+        travelForm.classList.add("hidden");
+        electricForm.classList.remove("hidden");
+        freightForm.classList.add("hidden");
+        starterPrompt.classList.add("hidden");
+        outerBox.style.display = "";
+        resultsWrapper.style.display = "";
     }
     if(inputMode == "food"){
-        travelForm.classList.add("hidden")
-        freightForm.classList.remove("hidden")
-        electricForm.classList.add("hidden")
+        travelForm.classList.add("hidden");
+        freightForm.classList.remove("hidden");
+        electricForm.classList.add("hidden");
+        starterPrompt.classList.add("hidden");
         outerBox.style.display = "";
+        resultsWrapper.style.display = "";
     }
 }
