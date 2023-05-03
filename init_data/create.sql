3969cf5f1e14cef262f0c2c97deb9c1835c32ce4
IanKeeler <iake8965@colorado.edu>
2023-05-03 13:14:35 -0600

    added user data

--- init_data/create.sql
+++ init_data/create.sql
@@ -69,22 +69,37 @@ CREATE TABLE household(
 -- pass1
 -- pass2
 -- pass3
-INSERT INTO users (username, user_password, user_weightfactor, user_carbonscore, user_description) VALUES ('user1', '$2b$10$4KoZfQeiD9MDlI5YRzMZtuBIJbXgjz.QqsZRmDG4NCfsXPSW4APWm', 1, 50, 'Dromaeosauridae (/ˌdrɒmi.əˈsɔːrɪdiː/) is a family of feathered theropod dinosaurs. They were generally small to medium-sized feathered carnivores that flourished in the Cretaceous Period. The name Dromaeosauridae means ''running lizards'', from Greek δρομαῖος (dromaîos), meaning ''running at full speed, swift'', and σαῦρος (saûros), meaning ''lizard''. In informal usage, they are often called raptors (after Velociraptor), a term popularized by the film Jurassic Park; several genera include the term "raptor" directly in their name, and popular culture has come to emphasize their bird-like appearance and speculated bird-like behavior.');
-INSERT INTO users (username, user_password, user_weightfactor, user_carbonscore, user_description) VALUES ('user2', '$2b$10$yPeUvrbER1W.Y/NGiffn8usAZFmltMhFB1BFo15jaBUX2dBcKzK76', 1, 27, 'Crocodylomorpha is a group of pseudosuchian archosaurs that includes the crocodilians and their extinct relatives. They were the only members of Pseudosuchia to survive the end-Triassic extinction. During Mesozoic and early Cenozoic times, crocodylomorphs were far more diverse than at Present. Triassic forms were small, lightly built, active terrestrial animals. During the Jurassic, Crocodylomorphs morphologically diversified into numerous niches, with the subgroups Neosuchia (which includes modern crocodilians) and the extinct Thalattosuchia adapting to aquatic life.');
-INSERT INTO users (username, user_password, user_weightfactor, user_carbonscore, user_description) VALUES ('user3', '$2b$10$SpdLRI.7Eb7qF94c44nI4eMWqTf75mHGqGTYb8X3FEZR0yAX/fp/6', 1, 16.8, '');
+-- pass4
+-- pass5
+INSERT INTO users (username, user_password, user_weightfactor, user_carbonscore, user_description) VALUES ('Ethan', '$2b$10$4KoZfQeiD9MDlI5YRzMZtuBIJbXgjz.QqsZRmDG4NCfsXPSW4APWm', 1, 519.5, 'Dromaeosauridae (/ˌdrɒmi.əˈsɔːrɪdiː/) is a family of feathered theropod dinosaurs. They were generally small to medium-sized feathered carnivores that flourished in the Cretaceous Period. The name Dromaeosauridae means ''running lizards'', from Greek δρομαῖος (dromaîos), meaning ''running at full speed, swift'', and σαῦρος (saûros), meaning ''lizard''. In informal usage, they are often called raptors (after Velociraptor), a term popularized by the film Jurassic Park; several genera include the term "raptor" directly in their name, and popular culture has come to emphasize their bird-like appearance and speculated bird-like behavior.');
+INSERT INTO users (username, user_password, user_weightfactor, user_carbonscore, user_description) VALUES ('Dayn', '$2b$10$yPeUvrbER1W.Y/NGiffn8usAZFmltMhFB1BFo15jaBUX2dBcKzK76', 1, 552.6, 'Crocodylomorpha is a group of pseudosuchian archosaurs that includes the crocodilians and their extinct relatives. They were the only members of Pseudosuchia to survive the end-Triassic extinction. During Mesozoic and early Cenozoic times, crocodylomorphs were far more diverse than at Present. Triassic forms were small, lightly built, active terrestrial animals. During the Jurassic, Crocodylomorphs morphologically diversified into numerous niches, with the subgroups Neosuchia (which includes modern crocodilians) and the extinct Thalattosuchia adapting to aquatic life.');
+INSERT INTO users (username, user_password, user_weightfactor, user_carbonscore, user_description) VALUES ('Ian', '$2b$10$SpdLRI.7Eb7qF94c44nI4eMWqTf75mHGqGTYb8X3FEZR0yAX/fp/6', 1, 678, 'The griffin is a fantastical creature steeped in myth and legend. It boasts the head, wings, and talons of an eagle, and the body of a lion. This magnificent beast is renowned for its fierce loyalty, protective nature, and regal presence. With its keen sense of sight and sharp claws, the griffin is a symbol of strength, courage, and vigilance. Its eagle-like head is adorned with a sharp beak, large eyes, and pointed ears, while its feathered wings are capable of flight. The griffins muscular body is covered in fur, and its four powerful legs end in razor-sharp talons. Often associated with royalty, the griffin is said to guard treasures and important locations such as castles and temples.');
+INSERT INTO users (username, user_password, user_weightfactor, user_carbonscore, user_description) VALUES ('Ben', '$2b$10$B/TOx/39o5Q4RQHoXLffnu2oNXSjWv2x2FgvAN0nqr6i2xx1XGFU6', 1, 533.8, '');
+INSERT INTO users (username, user_password, user_weightfactor, user_carbonscore, user_description) VALUES ('Joe', '$2b$10$ULB.uMWy8sxen7V251doo.aycXw5hwsiZvBNjydgHm5rEhW5GonQy', 1, 527.7, '');
+
 
 -- -----------------------------------------------------
 -- test data for leaderboard
 -- -----------------------------------------------------
 
-INSERT INTO travel (travel_mode, travel_distance, emissions, date, user_id) VALUES ('car', 16, 20, '2023-04-01', 1);
-INSERT INTO travel (travel_mode, travel_distance, emissions, date, user_id) VALUES ('car', 10, 10, '2023-04-01', 2);
-INSERT INTO travel (travel_mode, travel_distance, emissions, date, user_id) VALUES ('car', 26, 5, '2023-04-01', 3);
+INSERT INTO travel (travel_mode, travel_distance, emissions, date, user_id) VALUES ('walking', 5, 0, '2023-04-01', 1);
+INSERT INTO travel (travel_mode, travel_distance, emissions, date, user_id) VALUES ('car', 16, 5.27, '2023-04-01', 2);
+INSERT INTO travel (travel_mode, travel_distance, emissions, date, user_id) VALUES ('car', 40, 13.143, '2023-04-01', 3);
+INSERT INTO travel (travel_mode, travel_distance, emissions, date, user_id) VALUES ('bus', 20, 1.137, '2023-04-01', 4);
+INSERT INTO travel (travel_mode, travel_distance, emissions, date, user_id) VALUES ('biking', 10, 0, '2023-04-01', 5);
 
-/* INSERT INTO freight (freight_mode, freight_weight, freight_distance, emissions, date, user_id) VALUES ('road', 15, 25, 30, '2023-04-01', 1);
-INSERT INTO freight (freight_mode, freight_weight, freight_distance, emissions, date, user_id) VALUES ('road', 15, 25, 15, '2023-04-01', 2);
-INSERT INTO freight (freight_mode, freight_weight, freight_distance, emissions, date, user_id) VALUES ('road', 15, 25, 7, '2023-04-01', 3);
 
-INSERT INTO electricity (electricity_mode, electricity_used, emissions, date, user_id) VALUES ('charge device', 25, 10, '2023-04-01', 1);
-INSERT INTO electricity (electricity_mode, electricity_used, emissions, date, user_id) VALUES ('charge device', 25, 5, '2023-04-01', 2);
-INSERT INTO electricity (electricity_mode, electricity_used, emissions, date, user_id) VALUES ('charge device', 25, 2, '2023-04-01', 3); */
+INSERT INTO food (beef_bought, dairy_bought, fruits_bought, emissions, date, user_id) VALUES (20.00, 7.50, 15.00, 81.142, '2023-05-01', 1);
+INSERT INTO food (beef_bought, dairy_bought, fruits_bought, emissions, date, user_id) VALUES (35.00, 0.00, 30.00, 129.9, '2023-05-01', 2);
+INSERT INTO food (beef_bought, dairy_bought, fruits_bought, emissions, date, user_id) VALUES (80.00, 15.00, 15.00, 281.46, '2023-05-01', 3);
+INSERT INTO food (beef_bought, dairy_bought, fruits_bought, emissions, date, user_id) VALUES (42.00, 0.00, 15.00, 142.902, '2023-05-01', 4);
+INSERT INTO food (beef_bought, dairy_bought, fruits_bought, emissions, date, user_id) VALUES (33.00, 7.50, 15.00, 122.504, '2023-05-01', 5);
+
+
+INSERT INTO household (electricity_used, water_used, emissions, date, user_id) VALUES (20, 20, 100, '2023-04-01', 1);
+INSERT INTO household (electricity_used, water_used, emissions, date, user_id) VALUES (10, 10, 50, '2023-04-01', 2);
+INSERT INTO household (electricity_used, water_used, emissions, date, user_id) VALUES (30, 10, 89, '2023-04-01', 3);
+INSERT INTO household (electricity_used, water_used, emissions, date, user_id) VALUES (10, 30, 100, '2023-04-01', 4);
+INSERT INTO household (electricity_used, water_used, emissions, date, user_id) VALUES (10, 5, 37.5, '2023-04-01', 5);
+
+
